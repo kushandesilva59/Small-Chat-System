45e4a42b5006bc49ab71e925f8a4d776c351f63a
kushandesilva59 <kushandesilva801@gmail.com>
2022-08-13 12:20:43 +0530

    update content

--- src/Controller/ServerFormController.java
+++ src/Controller/ServerFormController.java
@@ -1,4 +1,36 @@
 package Controller;
 
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerFormController {
+    ServerSocket serverSocket;
+    public TextArea txtArea;
+    public TextField txtMassage;
+    final int PORT = 5001;
+    Socket socket;
+
+    public void initialize() throws IOException {
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                System.out.println("Server start");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            try {
+                socket = serverSocket.accept();
+                System.out.println("Client connected");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+    }
 }
