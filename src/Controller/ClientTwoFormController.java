b414db6934db2675fdb30fbc8d5bda4a7edeafa6
kushandesilva59 <kushandesilva801@gmail.com>
2022-08-13 19:59:52 +0530

    update content

---
+++ src/Controller/ClientTwoFormController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class ClientTwoFormController {
+
+    public AnchorPane clientTwoLoginContext;
+    public TextArea txtArea;
+    public TextField txtMessage;
+    Socket socket;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+    String message = "";
+
+    public void sendOnAction(ActionEvent event) {
+
+    }
+}
---
+++ src/Controller/ClientTwoLoginFormController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ClientTwoLoginFormController {
+
+    public AnchorPane clientTwoLoginContext;
+    public TextField txtUsername;
+    String username;
+
+    public void loginOnAction(ActionEvent event) {
+
+    }
+}
